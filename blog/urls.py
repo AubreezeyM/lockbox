d4156a2138463d03d393b41149f1ec5cadffae9b
Aubrey <aubreyminahan@gmail.com>
2025-07-04 23:05:51 -0400

    minor adjustment, link 'posts/' to ''

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.BlogPostList.as_view()),
+    path('', views.BlogPostList.as_view()),
     path('<int:pk>/', views.MarkdownView.as_view())
 ]
