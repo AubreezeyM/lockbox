4253f2f2f507fcfb5f797e7b8696d1c948e91fb9
Aubrey <aubreyminahan@gmail.com>
2025-07-07 12:20:39 -0400

    added a simple category system for use later

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.HomepageView.as_view()),
     path('posts/', views.BlogPostList.as_view()),
-    path('<int:pk>/', views.MarkdownView.as_view())
+    path('posts/<int:pk>/', views.MarkdownView.as_view())
 ]
